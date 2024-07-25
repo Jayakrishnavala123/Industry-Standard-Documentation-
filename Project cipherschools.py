aca146e4a284e86ebe05db6d6d59d457836a8740
Jayakrishnavala123 <Jayakrishnavala@gmail.com>
2024-07-25 20:14:18 +0530

    Add files via upload

---
+++ Project cipherschools.py
@@ -0,0 +1,77 @@
+
+import pandas as pd
+data = pd.read_csv("Mall_Customers.csv")
+# Display the first few rows of the dataset
+print(data.head(10))
+
+count=data.isnull().sum()
+print(count)
+
+mean_age=data['Age'].mean()
+data["Age"].fillna(mean_age,inplace=True)
+print(data.head(10))
+
+data.columns = ["CustomerID", "Gender", "Age", "AnnualIncome", "SpendingScore"]
+
+print(data)
+
+mode_gender=data['Gender'].mode()[0]
+type(mode_gender)
+mode_gender
+
+data.dropna(inplace=True)
+data["Gender"].fillna(mode_gender,inplace=True)
+
+print(data.head(20))
+
+count=data.isnull().sum()
+print(count)
+
+data['Gender'] = data['Gender'].map({'Male': 0, 'Female': 1})
+print(data)
+
+count=data.isnull().sum()
+print(count)
+
+print(data.describe())
+
+
+import  matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.figure(figsize=(10, 6))
+sns.histplot(data['Age'], bins=30, kde=True)
+plt.title('Age Distribution')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+sns.histplot(data['AnnualIncome'], bins=30, kde=True)
+plt.title('Annual Income Distribution')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+sns.histplot(data['SpendingScore'], bins=30, kde=True)
+plt.title('Spending Score Distribution')
+plt.show()
+
+
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=data, x='AnnualIncome', y='SpendingScore', hue='Gender')
+plt.title('Income vs Spending Score')
+plt.show()
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+features = data[['Age', 'AnnualIncome', 'SpendingScore']]
+
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(features)
+
+kmeans = KMeans(n_clusters=5, random_state=42)
+data['Cluster'] = kmeans.fit_predict(scaled_features)
+
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=data, x='AnnualIncome', y='SpendingScore', hue='Cluster', palette='viridis')
+plt.title('Customer Segments')
+plt.show()
